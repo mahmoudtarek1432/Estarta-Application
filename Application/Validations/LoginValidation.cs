b10cc076af0da3b4219b9360963d7676e7c74627
Mahmoud Tarek Naji <mtarek@valu.com.eg>
2026-02-01 11:02:17 +0200

    added fluent validation

---
+++ Application/Validations/LoginValidation.cs
@@ -0,0 +1,19 @@
+﻿using Application.DTOs;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Validations
+{
+    public class LoginValidation : AbstractValidator<LoginRequestDto>
+    {
+        public LoginValidation()
+        {
+            RuleFor(e => e.Email).NotEmpty().EmailAddress();
+            RuleFor(e => e.Password).NotEmpty().MinimumLength(8);
+        }
+    }
+}
